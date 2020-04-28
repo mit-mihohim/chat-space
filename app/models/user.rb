43de51c7b5677fa6f0873482307db776f17c300e
mit-mihohim <mihohim.jp@gmail.com>
2020-04-28 14:38:14 +0900

    validates mispelling changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-  validate :name, presence: true, uniqueness: true
+  validates :name, presence: true, uniqueness: true
 end
