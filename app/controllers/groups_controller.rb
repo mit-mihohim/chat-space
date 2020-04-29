c77d0e609b2665a67c55fd98ccce0d15289f745d
mit-mihohim <mihohim.jp@gmail.com>
2020-04-29 17:50:30 +0900

    added edit, update on groups_controller.rb
    グループコントローラーに編集ページへの遷移、編集アクション項目を追記
    ・編集アクションに対する分岐を作成
    →成功時: root_pathへ移動
    →失敗時: editページへ戻る

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -14,11 +14,19 @@ class GroupsController < ApplicationController
   end
 
   def edit
-    
+    @group = Group.find(params[:id])
   end
 
   def update
-    
+    @group = Group.find(params[:id])
+    if @group.update(group_params)
+      redirect_to root_path, notice: 'グループを更新しました'
+    else
+      render :edit
+    end
+  end
+
+  def index
   end
 
   private
